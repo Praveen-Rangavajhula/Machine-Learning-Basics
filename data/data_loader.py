434f2c3ff77d3d0f224c42d2ff31599d04b58659
Praveen-Rangavajhula <149396088+Praveen-Rangavajhula@users.noreply.github.com>
2024-10-11 15:40:53 -0400

    EDA for Titanic dataset for logistic regression and GDA

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,21 @@
+import os
+
 from sklearn.datasets import load_diabetes
 from scipy.stats import boxcox
 import pandas as pd
+
+def apply_box_cox_transformation(target_series):
+    """
+    Apply Box-Cox transformation to the target series.
+
+    Parameters:
+    target_series (Series): A pandas Series containing the target values to be transformed.
+
+    Returns:
+    Series: A pandas Series containing the transformed target values.
+    """
+    transformed_target, _, _ = boxcox(target_series, alpha=0.05)
+    return transformed_target
 
 def load_diabetes_data():
     """
@@ -22,15 +37,18 @@ def load_diabetes_data():
 
     return df_diabetes
 
-def apply_box_cox_transformation(target_series):
+def load_titanic_data(data_type='train'):
     """
-    Apply Box-Cox transformation to the target series.
+    Load Titanic dataset (train or test) based on the data_type parameter.
 
     Parameters:
-    target_series (Series): A pandas Series containing the target values to be transformed.
+    data_type (str): Type of data to load ('train' or 'test').
 
     Returns:
-    Series: A pandas Series containing the transformed target values.
+    DataFrame: A pandas DataFrame containing the Titanic dataset.
     """
-    transformed_target, _, _ = boxcox(target_series, alpha=0.05)
-    return transformed_target
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_name = 'train.csv' if data_type == 'train' else 'test.csv'
+    url = os.path.join(current_dir, '..', 'data', 'raw', 'titanic', file_name)
+    titanic_data = pd.read_csv(url)
+    return titanic_data
